b731a5292b2d5f476ca1c8a6f420c8c82310fe71
rachael baikey <rachaelacb@gmail.com>
2024-05-12 16:19:37 +0000

    secret key

--- booksys/settings.py
+++ booksys/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5h6x2$&^ib^l^dl7v#^j6whe_df^b2(k*58_te(o^f60(($qq2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
