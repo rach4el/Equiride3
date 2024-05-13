ab610cf36d81607904bb6e9ac417865843e061b1
rachael baikey <rachaelacb@gmail.com>
2024-05-13 14:03:26 +0000

    Add post model

--- booksys/settings.py
+++ booksys/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rach4el-equiride1-f29pwftkhs9.ws-eu111.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['http://8000-rach4el-equiride1-bu0fy1kihht.ws-eu111.gitpod.io/admin', '.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'booksys.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
